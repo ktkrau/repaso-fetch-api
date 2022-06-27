d7d1bed74826440758e46c2bfefac81fca9577ad
ktkrau <ktkrau@gmail.com>
2022-06-27 16:21:30 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//MANERA SINCRONICA
+/* const App = () => {
+
+    const [users, setUsers] = useState([]); 
+
+    useEffect(() => {
+        getUsers();
+
+    })
+    const getUsers = () => {
+       
+        fetch("https://jsonplaceholder.typicode.com/users", {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                console.log(response.ok);
+                return response.json();
+            }) 
+            .then((data) => {
+                console.log(data);
+                setUsers(data);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        console.log("hola")
+    }
+    return (
+        <>
+        <h1>App React</h1>
+        <ul>
+            {
+                users.length > 0 ?
+                users.map((user) => <li key={user.id}>{user.name}</li>): 
+                <li>Listado vacío</li>
+            }
+        </ul>
+        </>
+    )
+} */
+//MANERA ASINCRONICA
+const App = () => {
+
+    const [users, setUsers] = useState([]); 
+
+    useEffect(() => {
+        getUsersAsync();
+
+    })
+    const getUsersAsync = async () => {
+
+        try{
+          
+       
+       
+        const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response);
+
+        const info = await response.json();
+
+        console.log(info);
+
+        setUsers(info);
+}   catch (error) {
+    console.log(error);
+}
+        console.log("hola")
+    }
+    return (
+        <>
+        <h1>App React</h1>
+        <ul>
+            {
+                users.length > 0 ?
+                users.map((user) => <li key={user.id}>{user.name}</li>): 
+                <li>Listado vacío</li>
+            }
+        </ul>
+        </>
+    )
 }
 
-export default App;
+export default App; 
